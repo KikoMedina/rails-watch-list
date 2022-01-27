06ee44d7e9753254768acb2c14d9b663644901d3
KikoMedina <kotochie@hotmail.com>
2022-01-27 20:47:08 +0100

    Done Route, Model for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :list
+
+  validates :comment, presence: true
+  validates :rating, inclusion: { in: 0..10 }, numericality: { only_integer: true }
 end
